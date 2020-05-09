78bf7ab3c20843ab3c6fef6330fd5edeb7627a09
tunc bahcecioglu <tunc.bahcecioglu@gmail.com>
2020-05-09 17:53:21 +0200

    fixed RPc routing props

--- src/common/ImodelFileInterface.ts
+++ src/common/ImodelFileInterface.ts
@@ -1,11 +1,14 @@
-import { IModelRpcProps, RpcInterface, RpcManager } from "@bentley/imodeljs-common";
+import { IModelRpcProps, RpcInterface, RpcManager, RpcOperation, RpcRequestTokenSupplier_T } from "@bentley/imodeljs-common";
 import { IModelBasicDefinitions } from "./RpcTypes";
+
+const localDeploymentOnly: RpcRequestTokenSupplier_T = () => ({ iModelId: "none", key: "" });
 
 export abstract class ImodelFileInterface extends RpcInterface {
   public static readonly interfaceName = "ImodelFileInterface";
   public static interfaceVersion = "1.0.0";  // The API version of the interface
   public static getClient() { return RpcManager.getClientForInterface(ImodelFileInterface); }
 
+  @RpcOperation.setRoutingProps(localDeploymentOnly)
   public async createIModel(_filename: string): Promise<void> { return this.forward(arguments); }
   public async insertBasicDefinitions(_imodelTokenPropsProps: IModelRpcProps): Promise<IModelBasicDefinitions> { return this.forward(arguments); }
   public async addViewDefinition(_imodelTokenProps: IModelRpcProps, _basicDefinitions: IModelBasicDefinitions, _name: string): Promise<string> { return this.forward(arguments); }
